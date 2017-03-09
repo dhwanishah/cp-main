a327765fe9333fcbf621fa85083a8140781f5edf
Dhwani Shah <dshah202@gmail.com>
2017-03-09 11:21:55 -0500

    Removed enum since it's memory issue in android.

--- Week1/Flicks/app/src/main/java/com/dhwanishah/flicks/global/GlobalProperties.java
+++ Week1/Flicks/app/src/main/java/com/dhwanishah/flicks/global/GlobalProperties.java
@@ -11,25 +11,27 @@ public class GlobalProperties {
     private static final String MOVIE_DB_API_KEY = "?" + "a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     // Endpoints
-    private enum endpointType {
-        MOVIE_DB_NOWPLAYING_ENDPOINT("movie/now_playing");
+    private static final String MOVIE_DB_NOWPLAYING_ENDPOINT = "movie/now_playing";
 
-        private String stringValue;
-        endpointType(String toString) {
-            stringValue = toString;
-        }
-        @Override
-        public String toString() {
-            return stringValue;
-        }
-    }
+//    private enum endpointType {
+//        MOVIE_DB_NOWPLAYING_ENDPOINT("movie/now_playing");
+//
+//        private String endpoint;
+//        endpointType(String endpoint) {
+//            this.endpoint = endpoint;
+//        }
+//        @Override
+//        public String toString() {
+//            return endpoint;
+//        }
+//    }
 
 
 
     public static String generateMovieUrl(String type) {
         switch (type) {
-            case "movies_nowPlaying":
-                return MOVIE_DB_URL + endpointType.MOVIE_DB_NOWPLAYING_ENDPOINT + MOVIE_DB_API_KEY;
+            case "movie_nowPlaying":
+                return MOVIE_DB_URL + MOVIE_DB_NOWPLAYING_ENDPOINT + MOVIE_DB_API_KEY;
             default:
                 return "";
         }
