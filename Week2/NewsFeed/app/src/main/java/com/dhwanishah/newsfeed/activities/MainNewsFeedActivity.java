988ee93af33e7ce4fcc6f59981f0769717d22568
Dhwani Shah <dshah202@gmail.com>
2017-03-14 15:14:27 -0400

    Created custom toolbar and attached it.

---
+++ Week2/NewsFeed/app/src/main/java/com/dhwanishah/newsfeed/activities/MainNewsFeedActivity.java
@@ -0,0 +1,26 @@
+package com.dhwanishah.newsfeed.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.dhwanishah.newsfeed.R;
+
+public class MainNewsFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_news_feed);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_news_feed, menu);
+        return true;
+    }
+}
